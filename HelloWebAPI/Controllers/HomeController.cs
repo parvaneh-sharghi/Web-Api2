bf581397153cee592f15a282e8886e0e5e38e204
parvaneh-sharghi <parvaneh.sharghi@gmail.com>
2018-04-11 11:51:15 +0430

    commit3

--- HelloWebAPI/Controllers/HomeController.cs
+++ HelloWebAPI/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace HelloWebAPI.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            ViewBag.Title = "Web Api 2";
 
             return View();
         }
